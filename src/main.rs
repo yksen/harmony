c797924d9ba1ecb5eabe33e1af176ca21f53d2d8
xen <selegakamil@gmail.com>
2025-01-12 20:19:38 +0100

    Add `play` command

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,51 @@ use poise::serenity_prelude as serenity;
 use songbird::SerenityInit;
 use std::env;
 
-struct Data {}
+struct Data {
+    http_client: reqwest::Client,
+}
+
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
+/// Ping command
 #[poise::command(slash_command, prefix_command)]
 async fn ping(ctx: Context<'_>) -> Result<(), Error> {
     ctx.say("Pong").await?;
+    Ok(())
+}
+
+/// Play a song
+#[poise::command(slash_command, prefix_command, guild_only)]
+async fn play(ctx: Context<'_>, #[description = "YouTube URL"] query: String) -> Result<(), Error> {
+    let manager = songbird::get(ctx.serenity_context())
+        .await
+        .expect("Songbird Voice client has not been initialized")
+        .clone();
+
+    let guild = ctx.guild_id().unwrap();
+    let sender_channel = ctx
+        .guild()
+        .unwrap()
+        .voice_states
+        .get(&ctx.author().id)
+        .and_then(|voice_state| voice_state.channel_id);
+
+    if let Some(channel) = sender_channel {
+        if let Ok(handler_lock) = manager.join(guild, channel).await {
+            let mut handler = handler_lock.lock().await;
+        }
+
+        if let Some(handler_lock) = manager.get(guild) {
+            let mut handler = handler_lock.lock().await;
+            let source = songbird::input::YoutubeDl::new(ctx.data().http_client.clone(), query);
+            let _ = handler.play_input(source.clone().into());
+            ctx.say("Playing song").await?;
+        }
+    } else {
+        ctx.say("Not in a voice channel").await?;
+    }
+
     Ok(())
 }
 
@@ -23,13 +61,15 @@ async fn main() {
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![ping()],
+            commands: vec![ping(), play()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                Ok(Data {
+                    http_client: reqwest::Client::new(),
+                })
             })
         })
         .build();
